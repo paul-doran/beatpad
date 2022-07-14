1940bb27ca24627e0346fac933b4ec54e77ae7bc
paul-doran <104533011+paul-doran@users.noreply.github.com>
2022-07-14 16:52:20 -0500

    Updated code to allow Pad Bank sound changes
    When the pad bank is clicked the sounds will change to a new sound library.

--- index.js
+++ index.js
@@ -22,52 +22,98 @@ for (var i = 0; i < drumButtons; i++) {
   });
 
 
+// var padBank1 = [s d e r a w f t g]
+var padBank1 = ["sounds/Snare-13.wav", "sounds/Kick-8.wav", "sounds/HiHat-4.wav", "sounds/HiHat-3.wav", "sounds/Clap-6.wav", "sounds/Crash-10.wav", "sounds/Stabs-1.wav", "sounds/Chant-3.wav", "sounds/Snap-2.wav"];
+var padBank2 = ["sounds2/clp_can.wav", "sounds2/bd_isampleoldskool.wav", "sounds2/hat_classical.wav", "sounds2/prc_hit.wav", "sounds2/snr_dwreal.wav", "sounds2/ride_full.wav", "sounds2/shaker_bot.wav", "sounds2/tom_discodisco.wav", "sounds2/tom_taste.wav"];
+var clickCount = 0;
+
+document.getElementById("padBank").addEventListener("click", function() {
+
+clickCount += 1;
+var div2 = clickCount % 2;
+
+if (div2 == 1) {
+    padBank1 = padBank2;
+  }
+
+  else if (div2 == 0) {
+    padBank1 = ["sounds/Snare-13.wav", "sounds/Kick-8.wav", "sounds/HiHat-4.wav", "sounds/HiHat-3.wav", "sounds/Clap-6.wav", "sounds/Crash-10.wav", "sounds/Stabs-1.wav", "sounds/Chant-3.wav", "sounds/Snap-2.wav"];
+  }
+
+  makeAnimationPadBank();
+
+});
+
+// var keyBPressed = 0;
+// document.addEventListener("keydown", function(event) {
+//
+// keyBPressed++;
+// var keyPressed = event.key;
+// 
+// if (keyPressed === "b") {
+//   makeAnimationPadBank();
+// }
+//
+// var divs2 = keyBPressed % 2;
+// if (divs2 == 1) {
+//     padBank1 = padBank2;
+//   }
+//
+//   else if (divs2 == 0) {
+//     padBank1 = ["sounds/Snare-13.wav", "sounds/Kick-8.wav", "sounds/HiHat-4.wav", "sounds/HiHat-3.wav", "sounds/Clap-6.wav", "sounds/Crash-10.wav", "sounds/Stabs-1.wav", "sounds/Chant-3.wav", "sounds/Snap-2.wav"];
+//   }
+//
+// });
+
+
+
+
 
 
   function makeSound(key) {
   switch (key) {
     case "s":
-      var snare = new Audio("sounds/Snare-13.wav")
+      var snare = new Audio(padBank1[0])
       snare.play();
       break;
 
     case "d":
-      var kick = new Audio("sounds/Kick-8.wav")
+      var kick = new Audio(padBank1[1])
       kick.play();
       break;
 
     case "e":
-      var hiHatClosed = new Audio("sounds/HiHat-4.wav")
+      var hiHatClosed = new Audio(padBank1[2])
       hiHatClosed.play();
       break;
 
     case "r":
-      var hiHatOpen = new Audio("sounds/HiHat-3.wav")
+      var hiHatOpen = new Audio(padBank1[3])
       hiHatOpen.play();
       break;
 
     case "a":
-      var clap = new Audio("sounds/Clap-6.wav")
+      var clap = new Audio(padBank1[4])
       clap.play();
       break;
 
     case "w":
-      var crash = new Audio("sounds/Crash-10.wav")
+      var crash = new Audio(padBank1[5])
       crash.play();
       break;
 
     case "f":
-      var stab = new Audio("sounds/Stabs-1.wav")
+      var stab = new Audio(padBank1[6])
       stab.play();
       break;
 
       case "t":
-        var chant = new Audio("sounds/Chant-3.wav")
+        var chant = new Audio(padBank1[7])
         chant.play();
         break;
 
         case "g":
-          var snap = new Audio("sounds/Snap-2.wav")
+          var snap = new Audio(padBank1[8])
           snap.play();
           break;
 
@@ -93,3 +139,20 @@ buttonSelected.classList.remove("pressed");
 }
 
 }
+
+
+function makeAnimationPadBank() {
+
+  var buttonPushed = document.getElementById("padBank");
+
+  buttonPushed.id = "pressed";
+
+setTimeout(buttonRemove, 100);
+
+function buttonRemove() {
+buttonPushed.id = "padBank";
+
+}
+
+}
+
